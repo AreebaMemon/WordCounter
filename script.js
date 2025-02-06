d4b2c66975e8b0132c4a6be5e4537e9256ac8290
Areeba Memon <mareeba166@gmail.com>
2025-02-06 13:47:40 +0500

    AddLogic

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let textinput = document.querySelector("#textinput");
 let charactercount = document.querySelector("#characterCount");
 let wordcount = document.querySelector("#wordCount");
 let SentenceCount = document.querySelector("#SentenceCount");
+let VowelCount = document.querySelector("#VowelCount");
 
 textinput.addEventListener("input", () => {
   let text = textinput.value;
@@ -14,7 +15,29 @@ textinput.addEventListener("input", () => {
 
   let sentword = text.trim().split(".").length;
   SentenceCount.innerText = sentword-1;
-});
+
+  let count = 0;
+    for (let i = 0; i <text.length; i++) {
+      let char = text[i];
+      if (
+              char === "A" ||
+              char === "E" ||
+              char === "I" ||
+              char === "O" ||
+              char === "U" ||
+              char === "a" ||
+              char === "e" ||
+              char === "i" ||
+              char === "o" ||
+              char === "u"
+            ) {
+              count++;
+            }
+          }
+          
+          VowelCount.innerHTML=count;
+})
+
 
 let lower = document.querySelector("#lower");
 let upper = document.querySelector("#upper");
